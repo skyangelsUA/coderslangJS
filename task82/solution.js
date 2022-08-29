aab0a7148a2ddf5e8e50f773a2f7a02dff7d25d8
skyangelsUA <dkskyangels@gmail.com>
2022-08-30 00:29:42 +0300

    Задачи

---
+++ task82/solution.js
@@ -0,0 +1,1 @@
+let goodbyeMessage = "See you soon!";
---
+++ task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const iron = 1;
+const silver = 2;
+const gold = 3;
+const platinum = 4;
+const diamond = 5;
